20b0c4d24203f0bf607bc87fdef8352a1cbedf7c
BlushedNanis <luis_danieltg@outlook.com>
2024-04-04 14:28:27 -0400

    Implemented tabs to display the weather information to the user.

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
 import streamlit as st
-import plotly.express as px
+import pandas as pd
 from backend import get_forecast_data
 
 
-st.set_page_config(page_title="Weather Forecast", layout="wide")
+st.set_page_config(page_title="Weather Forecast")
 
-col1, col2 = st.columns(2)
 
-with col1:
 #Webapp body
-    st.title(body="Weather forecast for the next days", anchor=False)
-    st.divider()
-    place = st.text_input(label="Place:",
-                        placeholder="Type the name of the city",
-                        help="If the city name exists in different countries"\
-                            " add a comma followed by the country abbreviation"\
-                            " i.e. ,ca ,mx, us").capitalize()
-    days = st.slider(label="Forecast days", min_value=1, max_value=5,
-            help="This will be the number of forecasted days",
-            value=2)
-    option = st.selectbox(label="Select the data to view",
-                        options=("Temperature", "Sky"))
-    st.divider()
-    
-with col2:
-    try:
-        
-        if place:
-            data = get_forecast_data(place, days)
-            
-            if option == "Temperature":
-                temperatures = [dict["main"]["temp"] for dict in data]
-                dates = [dict["dt_txt"] for dict in data]
-                figure = px.line(x=dates, y=temperatures)
-                st.plotly_chart(figure_or_data=figure)
-                
-            if option == "Sky":
-                icons = {"Clear": "icons\\sunny.png", "Rain": "icons\\rainy.png",
-                        "Clouds": "icons\\cloudy.png", "Snow": "icons\\snowy.png"}
-                sky_conditions = [dict["weather"][0]["main"] for dict in data]
-                icons_paths = [icons[condition] for condition in sky_conditions]
-                st.image(icons_paths, width=115)
+st.title(body="Weather forecast for the next days", anchor=False)
+st.divider()
+place = st.text_input(label="Place:",
+                    placeholder="Type the name of the city",
+                    help="If the city name exists in different countries"\
+                        " add a comma followed by the country abbreviation"\
+                        " i.e. ,ca ,mx, us").capitalize()
+days = st.slider(label="Forecast days", min_value=1, max_value=5,
+        help="This will be the number of forecasted days",
+        value=3)
+option = st.selectbox(label="Select the data to view",
+                    options=("Temperature", "Sky"))
+st.divider()
 
-    except KeyError:
-        st.warning("Looks like you introduced a non-existing city, please try again.")
+
+tab1, tab2, tab3 = st.tabs([":thermometer: Temperatures Chart",
+                     ":mostly_sunny: Weather Conditions",
+                     ":cyclone: Wind Chart"])
+
+if place:
+    
+    weather, city = get_forecast_data(place, days)
+    
+    with tab1:
+        
+        temp_data = {"Temperatures": [dict["main"]["temp"] for dict in weather]}
+        temp_data["Dates"] = pd.to_datetime([dict["dt_txt"] for dict in weather])
+        temp_df = pd.DataFrame(data=temp_data)
+        st.line_chart(temp_df, x="Dates", y="Temperatures")
+                  
+    with tab2:
+        st.title("Test")
+        icons = {"Clear": "icons\\sunny.png", "Rain": "icons\\rainy.png",
+                "Clouds": "icons\\cloudy.png", "Snow": "icons\\snowy.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in weather]
+        icons_paths = [icons[condition] for condition in sky_conditions]
+        st.image(icons_paths, width=115)
+        
+    with tab3:
+        st.title("Test")
+        
+        '''        except KeyError:
+            st.warning("Looks like you introduced a non-existing city, please try again.")'''

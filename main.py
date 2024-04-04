634192db945b42e9b515c9fe04b1531b7f944136
BlushedNanis <luis_danieltg@outlook.com>
2024-04-03 23:32:29 -0400

    Added icons folder with weather images and display this images in webapp

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from backend import get_forecast_data
 
 #Webapp body
 st.title(body="Weather forecast for the next days", anchor=False)
-place = st.text_input(label="Place:", value="Toronto").capitalize()
+place = st.text_input(label="Place:",
+                      placeholder="Type the name of the city").capitalize()
 days = st.slider(label="Forecast days", min_value=1, max_value=5,
           help="This will be the number of forecasted days",
           value=2)
@@ -14,14 +15,20 @@ option = st.selectbox(label="Select the data to view",
 st.divider()
 st.header(body=f"{option} for the next {days} days in {place}", anchor=False)
 
-data = get_forecast_data(place, days)
 
-if option == "Temperature":
-    temperatures = [dict["main"]["temp"] for dict in data]
-    dates = [dict["dt_txt"] for dict in data]
-    figure = px.line(x=dates, y=temperatures)
-    st.plotly_chart(figure_or_data=figure)
+if place:
+    data = get_forecast_data(place, days)
     
-if option == "Sky":
-    sky = [dict["weather"][0]["main"] for dict in data]
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in data]
+        dates = [dict["dt_txt"] for dict in data]
+        figure = px.line(x=dates, y=temperatures)
+        st.plotly_chart(figure_or_data=figure)
+        
+    if option == "Sky":
+        icons = {"Clear": "icons\\sunny.png", "Rain": "icons\\rainy.png",
+                 "Clouds": "icons\\cloudy.png", "Snow": "icons\\snowy.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in data]
+        icons_paths = [icons[condition] for condition in sky_conditions]
+        st.image(icons_paths, width=115)
 
